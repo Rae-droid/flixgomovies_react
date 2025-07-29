61d57d7b284b6fd7ba20d8a3ea29e71f4033ba0c
rae-droid <chriscater523@gmail.com>
2025-07-29 21:19:48 +0000

    new backkend

--- SERVER/server.js
+++ SERVER/server.js
@@ -2,18 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import "./cronJobs/investmentPayout.js";
+
 
 // Routes
 import authRoutes from "./routes/auth.js";
-import walletRoutes from "./routes/wallet.js";
-import investmentRoutes from "./routes/investment.js";
-import dashboardRoutes from "./routes/dashboard.js";
-import transactionRoutes from "./routes/transactions.js";
+
 import userDataRoute from "./routes/userDataRoute.js";
 import User from "./models/User.js";
 import { requireAuth } from "./middleware/auth.js";
-import paystackRoutes from "./routes/paystack.js";
+
 
 dotenv.config();
 const app = express();
@@ -35,11 +32,7 @@ app.get("/api/isAuth", requireAuth, async (req, res) => {
   const user = await User.findById(req.userId);
   res.json({ message: "Access granted", user });
 });
-app.use("/api/wallet", walletRoutes);
-app.use("/api/investments", investmentRoutes);
-app.use("/api/dashboard", dashboardRoutes);
-app.use("/api/transactions", transactionRoutes);
-app.use("/api/paystack", paystackRoutes);
+
 
 app.use(express.json());
 
